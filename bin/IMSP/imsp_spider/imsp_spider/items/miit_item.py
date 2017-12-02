7a98efa930d861cec21e8038b05bc2e85717b177
wdong0472 <wangdong@kmopt.com>
2017-12-02 23:28:25 +0800

    add miit item

--- bin/IMSP/imsp_spider/imsp_spider/items/miit_item.py
+++ bin/IMSP/imsp_spider/imsp_spider/items/miit_item.py
@@ -12,4 +12,5 @@ import scrapy
 class MiitSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()    
